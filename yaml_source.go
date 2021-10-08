16e54d130f3c15b28a1101199a07abf4d3c8ff7e
Eitan Joffe <ejoffe@gmail.com>
2021-10-08 14:15:48 -0700

    add YamlMustFileSource that panics if file is not found
    commit-id:2bd55c03

--- yaml_source.go
+++ yaml_source.go
@@ -30,3 +30,25 @@ func (s *yamlSource) Load(configPtr interface{}) {
 	err = decoder.Decode(configPtr)
 	check(err)
 }
+
+func YamlMustFileSource(filepath string) *yamlMustSource {
+	return &yamlMustSource{
+		filepath: filepath,
+	}
+}
+
+type yamlMustSource struct {
+	filepath string
+}
+
+func (s *yamlMustSource) Load(configPtr interface{}) {
+	file, err := os.OpenFile(s.filepath, os.O_RDONLY, os.ModePerm)
+	if err != nil {
+		panic(fmt.Errorf("rake: config file %s not found", s.filepath))
+	}
+	defer file.Close()
+
+	decoder := yaml.NewDecoder(file)
+	err = decoder.Decode(configPtr)
+	check(err)
+}
