9b20823215ec7bc8c3e7c0738e4db66c4f873475
Adam Benhassen <adam@inigo.io>
2022-12-22 04:24:52 +0200

    replace omit with keep implementation
    commit-id:1ccd13ae

--- yaml_writer_test.go
+++ yaml_writer_test.go
@@ -10,70 +10,18 @@ import (
 
 func TestYamlWriterOmit(t *testing.T) {
 	type cfg struct {
-		Str      string `writer:"omit"`
-		StrD     string
-		EmptyStr string
-		Int      int64   `writer:"omit"`
-		Bool     bool    `writer:"omit"`
-		Ptr      *string `writer:"omit"`
-		Struct   struct {
-			Str string
-		}
-		StructPtr *struct {
-			Str *string
-		}
-	}
-
-	s := "something"
-	actual := cfg{
-		Str:  "prev",
-		StrD: "asmth",
-		Int:  123,
-		Bool: true,
-		Ptr:  &s,
-		Struct: struct{ Str string }{
-			Str: "Data",
-		},
-		StructPtr: &struct{ Str *string }{
-			Str: &s,
-		},
-	}
-
-	expected := actual
-
-	var result bytes.Buffer
-	ymw := &yamlWriter{output: &result}
-	ymw.Load(&actual)
-
-	require.YAMLEq(t, `str: ""
-strd: asmth
-int: 0
-bool: false
-emptystr: ""
-ptr: null
-struct:
-  str: Data
-structptr:
-  str: something
-`, result.String())
-
-	require.EqualValues(t, expected, actual)
-}
-
-func TestYamlWriterKeep(t *testing.T) {
-	type cfg struct {
-		Str        string `writer:"keep"`
-		StrCustom  string `yaml:"abc" writer:"keep"`
-		StrCustom2 string `yaml:"cba,omitempty" writer:"keep"`
-		Int        int    `writer:"readonly"`
-		Empty      string `yaml:"Version,omitempty" writer:"keep"`
+		Int        int    `writer:"omit"`
+		Str        string `writer:"omit"`
+		StrCustom  string `yaml:"abc" writer:"omit"`
+		StrCustom2 string `yaml:"cba,omitempty" writer:"omit"`
+		Empty      string `yaml:"Version,omitempty" writer:"omit"`
 	}
 
 	input := cfg{
+		Int:        1,
 		Str:        "Something",
 		StrCustom:  "Something2",
 		StrCustom2: "Something3",
-		Int:        1,
 	}
 	out, err := yaml.Marshal(input)
 	require.NoError(t, err)
